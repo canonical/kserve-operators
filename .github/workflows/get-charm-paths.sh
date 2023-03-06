835ce3469cac415d4cee3874c8166ce7b738b400
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-03-06 14:57:40 +0100

    ci: add reusable publish job (#66)
    * ci: add reusable publish job
    * ci: add FIXME message in publish job
    
    The kserve-web-app charm is not ready yet, thus the publish
    job is expected to fail.

---
+++ .github/workflows/get-charm-paths.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+# Finds the charms in this repo, outputting them as JSON
+# Will return one of:
+# * the relative paths of the directories listed in `./charms`, if that directory exists
+# * "./", if the root directory has a "metadata.yaml" file
+# * otherwise, error
+#
+# Modified from: https://stackoverflow.com/questions/63517732/github-actions-build-matrix-for-lambda-functions/63736071#63736071
+CHARMS_DIR="./charms"
+if [ -d "$CHARMS_DIR" ];
+then
+  CHARM_PATHS=$(find $CHARMS_DIR -maxdepth 1 -type d -not -path '*/\.*' -not -path "$CHARMS_DIR")
+else
+  if [ -f "./metadata.yaml" ]
+  then
+    CHARM_PATHS="./"
+  else
+    echo "Cannot find valid charm directories - aborting"
+    exit 1
+  fi
+fi
+
+# Convert output to JSON string format
+# { charm_paths: [...] }
+
+CHARM_PATHS_LIST=$(echo "$CHARM_PATHS" | jq -c --slurp --raw-input 'split("\n")[:-1]')
+
+# FIXME: kserve-web-app is not ready to be built and published, the publish job for
+# that charm is expected to fail. It can be ignored until the charm is not WIP.
+echo "Found CHARM_PATHS_LIST: $CHARM_PATHS_LIST (Only publishing kserve-controller)"
+
+echo "::set-output name=CHARM_PATHS_LIST::$CHARM_PATHS_LIST"
