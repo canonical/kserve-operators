e68c657ccc688e7f9674f0897a724e36f27fe450
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-04-24 14:09:53 +0200

    feat: enable serverless and rawdeployment deployment mode (#97)
    * feat: configure the deployment mode
    
    This commit enables different charm configurations and
    relations to be able to set up the deployment mode.

--- charms/kserve-controller/src/charm.py
+++ charms/kserve-controller/src/charm.py
@@ -21,10 +21,15 @@ from charmed_kubeflow_chisme.exceptions import ErrorWithStatus
 from charmed_kubeflow_chisme.kubernetes import KubernetesResourceHandler
 from charmed_kubeflow_chisme.lightkube.batch import delete_many
 from charmed_kubeflow_chisme.pebble import update_layer
+from charms.istio_pilot.v0.istio_gateway_info import (
+    GatewayRelationDataMissingError,
+    GatewayRelationMissingError,
+    GatewayRequirer,
+)
 from lightkube import ApiError
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, WaitingStatus
 from ops.pebble import Layer
 
 # from lightkube_custom_resources.serving import ClusterServingRuntime_v1alpha1
@@ -36,9 +41,8 @@ K8S_RESOURCE_FILES = [
     "src/templates/auth_manifests.yaml.j2",
     "src/templates/serving_runtimes_manifests.yaml.j2",
     "src/templates/webhook_manifests.yaml.j2",
-    "src/templates/configmap_manifests.yaml.j2",
 ]
-CONFIG_FILE = "src/config/inference-config.j2"
+CONFIG_FILES = ["src/templates/configmap_manifests.yaml.j2"]
 
 
 class KServeControllerCharm(CharmBase):
@@ -46,8 +50,11 @@ class KServeControllerCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
+        self._ingress_gateway_requirer = GatewayRequirer(self, relation_name="ingress-gateway")
+        self._local_gateway_requirer = GatewayRequirer(self, relation_name="local-gateway")
 
         self.framework.observe(self.on.install, self._on_install)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.remove, self._on_remove)
         self.framework.observe(
             self.on.kserve_controller_pebble_ready, self._on_kserve_controller_ready
@@ -55,9 +62,23 @@ class KServeControllerCharm(CharmBase):
         self.framework.observe(
             self.on.kube_rbac_proxy_pebble_ready, self._on_kube_rbac_proxy_ready
         )
+        self.framework.observe(
+            self.on["ingress-gateway"].relation_changed, self._on_ingress_gateway_relation_changed
+        )
+        # Observe if relation is removed by juju remove-application or juju remove-relation
+        self.framework.observe(
+            self.on["ingress-gateway"].relation_broken, self._on_ingress_gateway_relation_broken
+        )
+        self.framework.observe(
+            self.on["local-gateway"].relation_changed, self._on_local_gateway_relation_changed
+        )
+        self.framework.observe(
+            self.on["local-gateway"].relation_broken, self._on_local_gateway_relation_broken
+        )
 
         self._k8s_resource_handler = None
         self._crd_resource_handler = None
+        self._cm_resource_handler = None
         self._lightkube_field_manager = "lightkube"
         self._controller_container_name = "kserve-controller"
         self.controller_container = self.unit.get_container(self._controller_container_name)
@@ -77,6 +98,30 @@ class KServeControllerCharm(CharmBase):
         }
 
     @property
+    def _inference_service_context(self):
+        """Context for rendering the inferenceservive-config ConfigMap."""
+        # Ensure any input is valid for deployment mode
+        deployment_mode = self.model.config["deployment-mode"].lower()
+        if deployment_mode == "serverless":
+            deployment_mode = "Serverless"
+        elif deployment_mode == "rawdeployment":
+            deployment_mode = "RawDeployment"
+        else:
+            raise ErrorWithStatus(
+                "Please set deployment-mode to either Serverless or RawDeployment", BlockedStatus
+            )
+
+        inference_service_context = {
+            "ingress_domain": self.model.config["domain-name"],
+            "deployment_mode": deployment_mode,
+            "namespace": self.model.name,
+        }
+        # Generate and add gateway context
+        gateways_context = self._generate_gateways_context()
+        inference_service_context.update(gateways_context)
+        return inference_service_context
+
+    @property
     def k8s_resource_handler(self):
         """Returns an instance of the KubernetesResourceHandler."""
         if not self._k8s_resource_handler:
@@ -87,6 +132,18 @@ class KServeControllerCharm(CharmBase):
                 logger=log,
             )
         return self._k8s_resource_handler
+
+    @property
+    def cm_resource_handler(self):
+        """Returns an instance of the KubernetesResourceHandler."""
+        if not self._cm_resource_handler:
+            self._cm_resource_handler = KubernetesResourceHandler(
+                field_manager=self._lightkube_field_manager,
+                template_files=CONFIG_FILES,
+                context=self._inference_service_context,
+                logger=log,
+            )
+        return self._cm_resource_handler
 
     @property
     def _controller_pebble_layer(self):
@@ -123,6 +180,16 @@ class KServeControllerCharm(CharmBase):
                 }
             }
         )
+
+    @property
+    def _ingress_gateway_info(self):
+        """Returns the ingress gateway info."""
+        return self._ingress_gateway_requirer.get_relation_data()
+
+    @property
+    def _local_gateway_info(self):
+        """Returns the local gateway info."""
+        return self._local_gateway_requirer.get_relation_data()
 
     def _on_kserve_controller_ready(self, event):
         """Define and start a workload using the Pebble API.
@@ -185,23 +252,115 @@ class KServeControllerCharm(CharmBase):
         try:
             self.unit.status = MaintenanceStatus("Creating k8s resources")
             self.k8s_resource_handler.apply()
-        except ApiError as e:
-            log.error(e)
+            self.cm_resource_handler.apply()
+        except ErrorWithStatus as err:
+            self.model.unit.status = err.status
+            log.error(f"Failed to handle {event} with error: {err}")
+            return
+        except ApiError as api_err:
+            log.error(api_err)
             raise
-        self.model.unit.status = ActiveStatus()
+        else:
+            self.model.unit.status = ActiveStatus()
+
+    def _on_config_changed(self, event):
+        self._on_install(event)
 
     def _on_remove(self, _):
         self.unit.status = MaintenanceStatus("Removing k8s resources")
         k8s_resources_manifests = self.k8s_resource_handler.render_manifests()
+        cm_resources_manifests = self.cm_resource_handler.render_manifests()
         try:
             delete_many(
                 self.k8s_resource_handler.lightkube_client,
                 k8s_resources_manifests,
             )
+            delete_many(
+                self.cm_resource_handler.lightkube_client,
+                cm_resources_manifests,
+            )
+
         except ApiError as e:
             log.warning(f"Failed to delete resources, with error: {e}")
             raise e
         self.unit.status = MaintenanceStatus("K8s resources removed")
+
+    def _on_ingress_gateway_relation_changed(self, event) -> None:
+        """Handle the ingress-gateway relation changed event."""
+        # Just call the event handler that applies manifest files
+        self._on_install(event)
+
+    def _on_local_gateway_relation_changed(self, event) -> None:
+        """Handle the local-gateway relation changed event."""
+        # Just call the event handler that applies manifest files
+        self._on_install(event)
+
+    def _on_ingress_gateway_relation_broken(self, _) -> None:
+        """Handle the ingress-gateway relation broken event."""
+        # Ingress is always needed, so immediately go into BlockedStatus
+        self.unit.status = BlockedStatus("Please relate to istio-pilot:gateway-info")
+        return
+
+    def _on_local_gateway_relation_broken(self, _) -> None:
+        """Handle the local-gateway relation broken event."""
+        if self.model.config["deployment-mode"].lower() == "serverless":
+            self.unit.status = BlockedStatus("Please relate to knative-serving:local-gateway")
+        return
+
+    def _generate_gateways_context(self) -> dict:
+        """Generates the ingress context based on certain rules.
+
+        Returns:
+            gateways_context (dict): a dictionary of all the fields in the ingress
+                section of the inferenceservice-config
+        Raises:
+            GatewayRelationMissingError: if any of the required relations are missing
+            GatewayRelationDataMissingError: if relation data is missing or incomplete
+        """
+        # Get the ingress-gateway info. This should always be known by this charm.
+        try:
+            ingress_gateway_info = self._ingress_gateway_info
+        except GatewayRelationMissingError:
+            raise ErrorWithStatus("Please relate to istio-pilot:gateway-info", BlockedStatus)
+        except GatewayRelationDataMissingError:
+            log.error("Missing or incomplete ingress gateway data.")
+            raise ErrorWithStatus("Waiting for ingress gateway data.", WaitingStatus)
+
+        # A temporal context with values only from ingress gateway
+        # FIXME: the ingress_gateway_service_name is hardcoded in istio-pilot
+        # and that information is not shared through the relation
+        gateways_context = {
+            "ingress_gateway_name": ingress_gateway_info["gateway_name"],
+            "ingress_gateway_namespace": ingress_gateway_info["gateway_namespace"],
+            "ingress_gateway_service_name": "istio-ingressgateway-workload",
+            "local_gateway_name": "",
+            "local_gateway_namespace": "",
+            "local_gateway_service_name": "",
+        }
+
+        # Get the local-gateway info. This value should only
+        # be get and rendered in Serverless Mode.
+        if self.model.config["deployment-mode"].lower() == "serverless":
+            try:
+                local_gateway_info = self._local_gateway_info
+                # FIXME: the local_gateway_service_name is hardcoded in knative-serving
+                # and that information is not shared through the relation
+                gateways_context.update(
+                    {
+                        "local_gateway_name": local_gateway_info["gateway_name"],
+                        "local_gateway_namespace": ingress_gateway_info["gateway_namespace"],
+                        "local_gateway_service_name": "knative-local-gateway",
+                    }
+                )
+            except GatewayRelationMissingError:
+                raise ErrorWithStatus(
+                    "Please relate to knative-serving:local-gateway", BlockedStatus
+                )
+            except GatewayRelationDataMissingError:
+                log.error("Missing or incomplete local gateway data.")
+                raise ErrorWithStatus("Waiting for local gateway data.", WaitingStatus)
+
+        return gateways_context
 
     def gen_certs(self, namespace, service_name):
         """Generate certificates."""
--- charms/kserve-controller/tests/integration/test_charm.py
+++ charms/kserve-controller/tests/integration/test_charm.py
@@ -16,6 +16,9 @@ from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
+ISTIO_VERSION = "1.16/stable"
+KNATIVE_VERSION = "latest/edge"
+ISTIO_INGRESS_GATEWAY = "test-gateway"
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
 
@@ -26,6 +29,29 @@ async def test_build_and_deploy(ops_test: OpsTest):
 
     Assert on the unit status before any relations/configurations take place.
     """
+    # Deploy istio-operators for ingress configuration
+    await ops_test.model.deploy(
+        "istio-pilot",
+        channel=ISTIO_VERSION,
+        config={"default-gateway": ISTIO_INGRESS_GATEWAY},
+        trust=True,
+    )
+
+    await ops_test.model.deploy(
+        "istio-gateway",
+        application_name="istio-ingressgateway",
+        channel=ISTIO_VERSION,
+        config={"kind": "ingress"},
+        trust=True,
+    )
+    await ops_test.model.add_relation("istio-pilot", "istio-ingressgateway")
+    await ops_test.model.wait_for_idle(
+        ["istio-pilot", "istio-ingressgateway"],
+        raise_on_blocked=False,
+        status="active",
+        timeout=90 * 10,
+    )
+
     # build and deploy charm from local source folder
     charm = await ops_test.build_charm(".")
     resources = {
@@ -35,18 +61,117 @@ async def test_build_and_deploy(ops_test: OpsTest):
         "kube-rbac-proxy-image": METADATA["resources"]["kube-rbac-proxy-image"]["upstream-source"],
     }
     await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME, trust=True)
+    await ops_test.model.add_relation("istio-pilot", "kserve-controller")
+
     # issuing dummy update_status just to trigger an event
     async with ops_test.fast_forward():
         await ops_test.model.wait_for_idle(
             apps=[APP_NAME],
             status="active",
-            raise_on_blocked=True,
+            raise_on_blocked=False,
             timeout=1000,
         )
-        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+    assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
 
 def test_inference_service_raw_deployment(ops_test: OpsTest):
+    """Validates that an InferenceService can be deployed."""
+    # Identify testing namespace (same as model)
+    namespace = ops_test.model_name
+
+    # Instantiate a lightkube client
+    lightkube_client = lightkube.Client()
+
+    # Read InferenceService example and create namespaced resource
+    inference_service_resource = lightkube.generic_resource.create_namespaced_resource(
+        group="serving.kserve.io",
+        version="v1beta1",
+        kind="InferenceService",
+        plural="inferenceservices",
+        verbs=None,
+    )
+    inf_svc_yaml = yaml.safe_load(Path("./tests/integration/sklearn-iris.yaml").read_text())
+    inf_svc_object = lightkube.codecs.load_all_yaml(yaml.dump(inf_svc_yaml))[0]
+
+    # Create InferenceService from example file
+    @tenacity.retry(
+        wait=tenacity.wait_exponential(multiplier=1, min=1, max=15),
+        stop=tenacity.stop_after_delay(30),
+        reraise=True,
+    )
+    def create_inf_svc():
+        lightkube_client.create(inf_svc_object, namespace=namespace)
+
+    # Assert InferenceService state is Available
+    @tenacity.retry(
+        wait=tenacity.wait_exponential(multiplier=1, min=1, max=15),
+        stop=tenacity.stop_after_attempt(10),
+        reraise=True,
+    )
+    def assert_inf_svc_state():
+        inf_svc = lightkube_client.get(
+            inference_service_resource, "sklearn-iris", namespace=namespace
+        )
+        conditions = inf_svc.get("status", {}).get("conditions")
+        for condition in conditions:
+            if condition.get("status") == "False":
+                status_overall = False
+                break
+            status_overall = True
+        assert status_overall is True
+
+    create_inf_svc()
+    assert_inf_svc_state()
+
+    # Remove the old InferenceService
+    lightkube_client.delete(inference_service_resource, name="sklearn-iris", namespace=namespace)
+
+
+async def test_deploy_knative_dependencies(ops_test: OpsTest):
+    """Deploy knative-operators as dependencies for serverless mode."""
+    # Deploy knative for serverless mode
+    namespace = ops_test.model_name
+
+    # Deploy knative-operators
+    await ops_test.model.deploy(
+        "knative-operator",
+        channel=KNATIVE_VERSION,
+        trust=True,
+    )
+    await ops_test.model.deploy(
+        "knative-serving",
+        channel=KNATIVE_VERSION,
+        config={
+            "namespace": "knative-serving",
+            "istio.gateway.namespace": namespace,
+            "istio.gateway.name": ISTIO_INGRESS_GATEWAY,
+        },
+        trust=True,
+    )
+    await ops_test.model.wait_for_idle(
+        ["knative-operator", "knative-serving"],
+        raise_on_blocked=False,
+        status="active",
+        timeout=90 * 10,
+    )
+
+    # Relate kserve-controller and knative-serving
+    await ops_test.model.add_relation("knative-serving", "kserve-controller")
+
+    # Change deployment mode to Serverless
+    await ops_test.model.applications["kserve-controller"].set_config(
+        {"deployment-mode": "serverless"}
+    )
+
+    await ops_test.model.wait_for_idle(
+        ["kserve-controller"],
+        raise_on_blocked=False,
+        status="active",
+        timeout=90 * 10,
+    )
+
+
+def test_inference_service_serverless_deployment(ops_test: OpsTest):
     """Validates that an InferenceService can be deployed."""
     # Identify testing namespace (same as model)
     namespace = ops_test.model_name
