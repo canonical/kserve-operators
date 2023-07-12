a7577a8aa8955eb33349e5acb26fce520dfd4cc6
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-12 13:27:25 -0400

    feat: added scan of metadata images

--- tools/get-images-1.7-stable.sh
+++ tools/get-images-1.7-stable.sh
@@ -8,6 +8,7 @@ STATIC_IMAGE_LIST=(
 # dynamic list
 git checkout origin/track/0.10
 IMAGE_LIST=()
+IMAGE_LIST+=($(find $REPO -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
 IMAGE_LIST+=($(grep image charms/kserve-controller/src/templates/serving_runtimes_manifests.yaml.j2 | awk '{print $2}' | sort --unique))
 IMAGE_LIST+=($(grep image charms/kserve-controller/src/templates/configmap_manifests.yaml.j2 | awk '{print $3}' | sort --unique | sed s/,//g | sed s/\"//g))
 
