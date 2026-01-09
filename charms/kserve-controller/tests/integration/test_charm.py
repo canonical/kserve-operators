766741457429df6d9a340986fc6fe4cc92bd0bc3
Dario Faccin <35623244+dariofaccin@users.noreply.github.com>
2026-01-09 13:30:51 +0100

    feat: run workload and charm as unprivileged user (#410)
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- charms/kserve-controller/tests/integration/test_charm.py
+++ charms/kserve-controller/tests/integration/test_charm.py
@@ -20,8 +20,11 @@ from charmed_kubeflow_chisme.testing import (
     assert_alert_rules,
     assert_logging,
     assert_metrics_endpoint,
+    assert_security_context,
     deploy_and_assert_grafana_agent,
+    generate_container_securitycontext_map,
     get_alert_rules,
+    get_pod_names,
 )
 from charms_dependencies import (
     ISTIO_GATEWAY,
@@ -33,6 +36,7 @@ from charms_dependencies import (
     RESOURCE_DISPATCHER,
 )
 from jinja2 import Template
+from lightkube import Client
 from lightkube.core.exceptions import ApiError
 from lightkube.models.meta_v1 import ObjectMeta
 from lightkube.resources.core_v1 import (
@@ -61,13 +65,13 @@ CONFIGMAP_DATA_INGRESS_GATEWAY_NAMESPACE = "kubeflow"
 CONFIGMAP_DATA_INGRESS_GATEWAY_NAME = "test-gateway"
 
 MANIFESTS_SUFFIX = "-s3"
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 NAMESPACE_FILE = "./tests/integration/namespace.yaml"
 TESTING_LABELS = ["user.kubeflow.org/enabled"]
 ISTIO_INGRESS_GATEWAY = "test-gateway"
 ISTIO_GATEWAY_APP_NAME = "istio-ingressgateway"
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
+CONTAINERS_SECURITY_CONTEXT_MAP = generate_container_securitycontext_map(METADATA)
 CONFIGMAP_NAME = "inferenceservice-config"
 PODDEFAULTS_CRD_TEMPLATE = "./tests/integration/crds/poddefaults.yaml"
 
@@ -253,8 +257,9 @@ def serverless_namespace(lightkube_client):
 
 
 @pytest.fixture(scope="session")
-def lightkube_client() -> lightkube.Client:
-    client = lightkube.Client(field_manager="kserve")
+def lightkube_client() -> Client:
+    """Returns lightkube Kubernetes client"""
+    client = Client(field_manager=f"{APP_NAME}")
     return client
 
 
@@ -376,7 +381,7 @@ async def test_logging(ops_test: OpsTest):
     await assert_logging(app)
 
 
-async def test_metrics_enpoint(ops_test):
+async def test_metrics_endpoint(ops_test: OpsTest):
     """Test metrics_endpoints are defined in relation data bag and their accessibility.
     This function gets all the metrics_endpoints from the relation data bag, checks if
     they are available from the grafana-agent-k8s charm and finally compares them with the
@@ -386,7 +391,7 @@ async def test_metrics_enpoint(ops_test):
     await assert_metrics_endpoint(app, metrics_port=8080, metrics_path="/metrics")
 
 
-async def test_alert_rules(ops_test):
+async def test_alert_rules(ops_test: OpsTest):
     """Test check charm alert rules and rules defined in relation data bag."""
     app = ops_test.model.applications[APP_NAME]
     alert_rules = get_alert_rules()
@@ -679,3 +684,24 @@ async def test_blocked_on_invalid_config(ops_test: OpsTest):
         apps=[APP_NAME], status="blocked", raise_on_blocked=False, timeout=300
     )
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "blocked"
+
+
+@pytest.mark.parametrize("container_name", list(CONTAINERS_SECURITY_CONTEXT_MAP.keys()))
+async def test_container_security_context(
+    ops_test: OpsTest,
+    lightkube_client: Client,
+    container_name: str,
+):
+    """Test container security context is correctly set.
+
+    Verify that container spec defines the security context with correct
+    user ID and group ID.
+    """
+    pod_name = get_pod_names(ops_test.model.name, APP_NAME)[0]
+    assert_security_context(
+        lightkube_client,
+        pod_name,
+        container_name,
+        CONTAINERS_SECURITY_CONTEXT_MAP,
+        ops_test.model.name,
+    )
