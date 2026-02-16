f4361c6c94b3030d5253064f93663ba0e5b964a5
Dario Faccin <dario.faccin@canonical.com>
2026-02-16 15:25:24 +0100

    chore: pin dependencies version
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- charms/kserve-controller/tests/integration/charms_dependencies.py
+++ charms/kserve-controller/tests/integration/charms_dependencies.py
@@ -7,15 +7,15 @@ ISTIO_GATEWAY = CharmSpec(
     charm="istio-gateway", channel="latest/edge", config={"kind": "ingress"}, trust=True
 )
 ISTIO_PILOT = CharmSpec(charm="istio-pilot", channel="latest/edge", trust=True)
-KNATIVE_OPERATOR = CharmSpec(charm="knative-operator", channel="latest/edge", trust=True)
-KNATIVE_SERVING = CharmSpec(charm="knative-serving", channel="latest/edge", trust=True)
+KNATIVE_OPERATOR = CharmSpec(charm="knative-operator", channel="1.16/stable", trust=True)
+KNATIVE_SERVING = CharmSpec(charm="knative-serving", channel="1.16/stable", trust=True)
 METACONTROLLER_OPERATOR = CharmSpec(
-    charm="metacontroller-operator", channel="latest/edge", trust=True
+    charm="metacontroller-operator", channel="4.11/stable", trust=True
 )
 MINIO = CharmSpec(
     charm="minio",
-    channel="latest/edge",
+    channel="ckf-1.10/stable",
     trust=True,
     config={"access-key": "minio", "secret-key": "minio-secret-key"},
 )
-RESOURCE_DISPATCHER = CharmSpec(charm="resource-dispatcher", channel="latest/edge", trust=True)
+RESOURCE_DISPATCHER = CharmSpec(charm="resource-dispatcher", channel="2.0/stable", trust=True)
