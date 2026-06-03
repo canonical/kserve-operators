ea57e633a8ab4ca52d17c307bca4170b55787721
mvlassis <manos.vlassis@canonical.com>
2026-06-03 16:20:07 +0300

    fix: pass charm path to integration tests

--- charms/kserve-controller/tests/integration/test_charm.py
+++ charms/kserve-controller/tests/integration/test_charm.py
@@ -6,7 +6,6 @@
 import base64
 import json
 import logging
-import os
 import time
 from pathlib import Path
 
@@ -234,7 +233,7 @@ def lightkube_client() -> lightkube.Client:
 
 @pytest.mark.skip_if_deployed
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
+async def test_build_and_deploy(ops_test: OpsTest, request):
     """Build the charm-under-test and deploy it together with related charms.
 
     Assert on the unit status before any relations/configurations take place.
@@ -263,7 +262,11 @@ async def test_build_and_deploy(ops_test: OpsTest):
     )
 
     # build and deploy charm from local source folder
-    entity_url = os.environ.get("CHARM_PATH") or await ops_test.build_charm(".")
+    entity_url = (
+        await ops_test.build_charm(".")
+        if not (entity_url := request.config.getoption("--charm-path"))
+        else entity_url
+    )
     resources = {
         "kserve-controller-image": METADATA["resources"]["kserve-controller-image"][
             "upstream-source"
